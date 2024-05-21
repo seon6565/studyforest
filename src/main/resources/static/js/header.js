c8fc69aa88497ad9b3bf73165ee3b83d9d02ceef
JS <wow220713@gmail.com>
2024-05-21 13:14:57 +0900

    version1.1

--- src/main/resources/static/js/header.js
+++ src/main/resources/static/js/header.js
@@ -8,7 +8,7 @@ function header(){
         <div class="collapse navbar-collapse" id="ftco-nav">
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item active"><a href="/" class="nav-link pl-0">Home</a></li>
-                <li class="nav-item"><a href="/bbs" class="nav-link">전체게시판</a></li>
+                <li class="nav-item"><a href="/bbs" class="nav-link">자유게시판</a></li>
                 <li class="nav-item"><a href="/member/regist" class="nav-link">회원가입</a></li>
                 <li class="nav-item"><a href="/autologin" class="nav-link">로그인</a></li>
             </ul>
